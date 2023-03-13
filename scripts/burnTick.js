b5de65c1960e65bcdbc381d744f350443a11efe1
zhangxiaobin <zhangxiaobin@gelaxy.io>
2023-03-13 11:35:18 +0800

    burn tick use bigint

--- scripts/burnTick.js
+++ scripts/burnTick.js
@@ -28,7 +28,7 @@ const main = async () => {
     let saddress = createSaddress(publicKey)
     console.log("saddress", saddress)
 
-    let tokenID = web3.utils.hexToBytes("0x78c1645758228af7255c596cdc276d95ce47b52533b0bf14bd0136cf61560f01")
+    let tokenID = BigInt("0x78c1645758228af7255c596cdc276d95ce47b52533b0bf14bd0136cf61560f01")
     let tx = await nftContract.approve(stakeSticket.address, tokenID);
     await sleep(10000)
     console.log("approve tx.hash = ", tx.hash);
