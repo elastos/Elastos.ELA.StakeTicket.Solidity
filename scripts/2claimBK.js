8b60e7ff6aedd5489635a3a8949fb6c40e29923b
zhangxiaobin <zhangxiaobin@gelaxy.io>
2023-03-23 08:49:06 +0800

    erc721 add changeOwner

--- scripts/2claimBK.js
+++ scripts/2claimBK.js
@@ -23,34 +23,40 @@ const main = async () => {
     console.log("setMinerRole tx.hash", tx.hash)
     await sleep(10000)
 
-    let elaHash="0x78c1645758228af7255c596cdc276d95ce47b52533b0bf14bd0136cf61560f01"
-    let ecdsa = ECDSA.generateKey("9812de27b49fff142b49da18396c09e99bb47abdf58c983335c51fdefd1c3c", "prime256v1")
-    let data =  web3.utils.hexToBytes(elaHash)
-    let signature = ecdsa.sign(Buffer.from(data), "hex");
-    console.log("signature", signature, "length", signature.length)
-    let publicKey = ecdsa.toCompressedPublicKey("hex");
-    console.log("getPublicKey", publicKey);
-    console.log("verify", ecdsa.verify(Buffer.from(data), signature, "hex"));
+    let  Role = web3.utils.keccak256("MINTER_ROLE");
+    Role = web3.utils.hexToBytes(Role);
+    let count = await nftContract.getRoleMemberCount(Role);
+    let owner = await nftContract.getRoleMember(Role, 0);
+    console.log("behind setMinterRole2", "count", count, "owner", owner);
 
-    console.log("xxl before claim start : ");
-    tx = await stakeSticket.claim(
-        elaHash, Buffer.from(signature, "hex"), Buffer.from(publicKey, "hex"),
-        {
-            gasPrice: 0x02540be400,
-            gasLimit: 0x7a1200
-        }
-    );
-    //tx = await stakeSticket.claim(elaHash, signature, publicKey);
-    console.log("xxl before claim end : ");
-    console.log("claim tx", tx.hash)
-    await sleep(10000)
-
-    let balance = await nftContract.balanceOf(deployer.address)
-    console.log("balance of nft", balance)
-    let tokenID = await nftContract.tokenByIndex(0)
-    console.log("tokenID of nft", tokenID)
-    let ownerOf = await nftContract.ownerOf(tokenID)
-    console.log("ownerOf of nft", ownerOf)
+    // let elaHash="0x78c1645758228af7255c596cdc276d95ce47b52533b0bf14bd0136cf61560f01"
+    // let ecdsa = ECDSA.generateKey("9812de27b49fff142b49da18396c09e99bb47abdf58c983335c51fdefd1c3c", "prime256v1")
+    // let data =  web3.utils.hexToBytes(elaHash)
+    // let signature = ecdsa.sign(Buffer.from(data), "hex");
+    // console.log("signature", signature, "length", signature.length)
+    // let publicKey = ecdsa.toCompressedPublicKey("hex");
+    // console.log("getPublicKey", publicKey);
+    // console.log("verify", ecdsa.verify(Buffer.from(data), signature, "hex"));
+    //
+    // console.log("xxl before claim start : ");
+    // tx = await stakeSticket.claim(
+    //     elaHash, Buffer.from(signature, "hex"), Buffer.from(publicKey, "hex"),
+    //     {
+    //         gasPrice: 0x02540be400,
+    //         gasLimit: 0x7a1200
+    //     }
+    // );
+    // //tx = await stakeSticket.claim(elaHash, signature, publicKey);
+    // console.log("xxl before claim end : ");
+    // console.log("claim tx", tx.hash)
+    // await sleep(10000)
+    //
+    // let balance = await nftContract.balanceOf(deployer.address)
+    // console.log("balance of nft", balance)
+    // let tokenID = await nftContract.tokenByIndex(0)
+    // console.log("tokenID of nft", tokenID)
+    // let ownerOf = await nftContract.ownerOf(tokenID)
+    // console.log("ownerOf of nft", ownerOf)
 
 
 }
